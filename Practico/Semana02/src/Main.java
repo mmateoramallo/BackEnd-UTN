5ccd3903df298da9d131aa574f37cf2015589496
Mateo <mateo.j.ramallo@gmail.com>
2024-08-28 16:08:43 -0300

    Segundo Apunte

--- Practico/Semana02/src/Main.java
+++ Practico/Semana02/src/Main.java
@@ -1,11 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        //Call function to add two numbers
-        System.out.println(add(5, 10));
-    }
-    //Create a function that's add two numbers
-    public static int add(int a, int b) {
-        return a + b;
+
     }
 }
